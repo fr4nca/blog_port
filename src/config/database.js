a2a98fe27818c7e0b1e75dbbe480ed6ad431968d
Victor França <victor.franca@confi.gr>
2019-10-21 14:56:18 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'fr4nca',
+  password: 'v1ct0r#fr4nc4',
+  database: 'victorfranca',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
